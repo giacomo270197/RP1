eb02eddc28f2b7fa7a451260c76ab86366600750
Giacomo <gcasoni@hotmail.it>
2020-01-11 20:40:40 +0100

    Added test analysis on core

--- anomaly_detection_tool/core.py
+++ anomaly_detection_tool/core.py
@@ -1,7 +1,7 @@
 import sys
 import statistics
 import json
-import io
+import os
 import lzma
 
 
@@ -18,12 +18,16 @@ class Prototypes:
 
     def __init__(self):
         self.prototypes = {}
+        self.log_file = open("results", "w")
+
+    def __del__(self):
+        self.log_file.close()
 
     def interval_update(self, prototype, interval):
         if len(prototype["intervals"]):
             prototype["intervals"].append(interval - prototype["intervals"][-1])
-            prototype["interval_mean"] = statistics.mean(prototype["intervals"])
-            prototype["interval_stdev"] = statistics.stdev(prototype["intervals"])
+            prototype["interval_mean"] = statistics.mean(prototype["intervals"][-100:])
+            prototype["interval_stdev"] = statistics.stdev(prototype["intervals"][-100:])
         else:
             prototype["intervals"].append(interval)
             prototype["interval_mean"] = statistics.mean(2 * prototype["intervals"])
@@ -34,9 +38,9 @@ class Prototypes:
         if location in prototype["locations_info"]:
             prototype["locations_info"][location]["intervals"].append(interval)
             prototype["locations_info"][location]["interval_mean"] = statistics.mean(
-                prototype["locations_info"][location]["intervals"])
+                prototype["locations_info"][location]["intervals"][-100:])
             prototype["locations_info"][location]["interval_stdev"] = statistics.stdev(
-                prototype["locations_info"][location]["intervals"])
+                prototype["locations_info"][location]["intervals"][-100:])
         else:
             prototype["locations_info"][location] = {
                 "interval_mean": statistics.mean(2 * [interval]),
@@ -49,9 +53,9 @@ class Prototypes:
         if computer in prototype["computers_info"]:
             prototype["computers_info"][computer]["intervals"].append(interval)
             prototype["computers_info"][computer]["interval_mean"] = statistics.mean(
-                prototype["computers_info"][computer]["intervals"])
+                prototype["computers_info"][computer]["intervals"][-100:])
             prototype["computers_info"][computer]["interval_stdev"] = statistics.stdev(
-                prototype["computers_info"][computer]["intervals"])
+                prototype["computers_info"][computer]["intervals"][-100:])
         else:
             prototype["computers_info"][computer] = {
                 "interval_mean": statistics.mean(2 * [interval]),
@@ -63,6 +67,32 @@ class Prototypes:
         self.interval_update(prototype, parsed[0])
         self.location_update(prototype, parsed[3], parsed[0])
         self.computer_update(prototype, parsed[2], parsed[0])
+
+    def file_compress(self, string, name):
+        filename = "tmp_file{}".format(name)
+        file = open(filename, "w")
+        file.write(string)
+        file.close()
+        initial_size = os.stat("tmp_file{}".format(name)).st_size
+        os.system("lzma -z {}".format(filename))
+        new_size = os.stat("{}.lzma".format(filename)).st_size
+        os.remove("{}.lzma".format(filename))
+        return (100 / initial_size) * new_size
+
+    def analyze_global_intervals(self, prototype, parsed):
+        if len(prototype["intervals"]) < 100:
+            return 0, 0
+        # Considering 2 stdev as limit of possible lottery
+        c_w = 100 / prototype["interval_stdev"] * abs(
+            prototype["interval_mean"] - (parsed[0] - prototype["intervals"][-1]))
+        c_d = self.file_compress(",".join([str(x) for x in prototype["intervals"][-100:]]), "intervals")
+        return c_w, c_d
+
+    def analyze(self, prototype, parsed):
+        x, y = self.analyze_global_intervals(prototype, parsed)
+        self.log_file.write("Cw: {}, ".format(str(x)))
+        self.log_file.write("Cd: {}, ".format(str(y)))
+        self.log_file.write("U: {}\n".format(str(x - y)))
 
     def generate_prototype(self, parsed):
         return {
@@ -91,11 +121,11 @@ def analyze(line, parser, prototypes):
     prototypes.update(prototype, parsed)
 
 
-filename = sys.argv[1]
-file = open(filename, 'r')
-parser = Parser()
-prototypes = Prototypes()
-for line in file:
-    analyze(line, parser, prototypes)
-
-print(json.dumps(prototypes.prototypes["U1"], indent=2))
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    file = open(filename, 'r')
+    parser = Parser()
+    prototypes = Prototypes()
+    for line in file:
+        analyze(line, parser, prototypes)
+    del prototypes
